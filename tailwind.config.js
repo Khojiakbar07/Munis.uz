c95ef69fc8a9aad5b1df521877045812d03fa781
Zokirov Xojiakbar <hojiakbar312@gmail.com>
2025-04-07 23:26:11 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        screens: {
+          'max-541': {'max': '541px'},
+          'max-413': {'max': '412px'},
+          'max-391': {'max': '391px'},
+          'max-376': {'max': '376px'},
+          'max-361': {'max': '361px'},
+        },
+      },
+    },
+    plugins: [],
+  }
